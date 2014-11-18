6de606333f091914bca408a4cf01eebbc4245e06
Tomas Karabela <tkarabela@seznam.cz>
2014-11-18 16:11:57 +0100

    Wages - make links bidirectional, enforce tree structure, count node depth

--- wages.py
+++ wages.py
@@ -6,6 +6,7 @@ Wages
 Features:
 object-oriented programming
 recursion
+tree traversal
 prettyprinting
 
 """
@@ -14,6 +15,7 @@ class Employee:
     def __init__(self, name):
         self.name = name
         self.subordinates = set() # cannot write empty set as {}, that would be an empty dict
+        self.boss = None
 
     def wage(self):
         n = self.count_subordinates()
@@ -27,8 +29,18 @@ class Employee:
         else:
             return 40000
 
+    def add_subordinates(self, *people):
+        for person in people:
+            if person in self.subordinates:
+                raise ValueError("Person %s is already a subordinate" % person.name)
+            elif person.boss is not None:
+                raise ValueError("Person %s has a different boss (%s)" % (person.name, person.boss.name))
+            else:
+                person.boss = self
+                self.subordinates.add(person)
+
     def count_subordinates(self):
-        # this only works when any person has at most one direct boss,
+        # this only works when any person has at most one direct boss (which is our case),
         # otherwise we would count them separately for each boss
 
         n = len(self.subordinates) # direct subordinates
@@ -37,6 +49,12 @@ class Employee:
             n += person.count_subordinates() # indirect subordinates
 
         return n
+
+    def count_superiors(self):
+        if self.boss is None:
+            return 0
+        else:
+            return self.boss.count_superiors() + 1
 
     def print_hierarchy(self, level=0):
         print(level*"\t", "-", self.name)
@@ -52,12 +70,13 @@ def main():
     gerry = Employee("Gerry")
     tom = Employee("Tom")
     paul = Employee("Paul")
+    ronald = Employee("Ronald")
     irwin = Employee("Irwin")
 
-    bill.subordinates.update({anne, mary, morgan})
-    mary.subordinates.update({tom, paul})
-    morgan.subordinates.update({irwin, paul, tom})
-    tom.subordinates.update({gerry})
+    bill.add_subordinates(anne, mary, morgan)
+    mary.add_subordinates(tom, ronald)
+    morgan.add_subordinates(irwin, paul)
+    tom.add_subordinates(gerry)
 
     for employee in [bill, anne, mary, morgan, gerry, tom, paul, irwin]:
         print(employee.name, "earns", employee.wage(), "CZK and has", employee.count_subordinates(), "subordinates")
@@ -67,6 +86,10 @@ def main():
 
     print("\nHierarchy from Tom's point of view:")
     tom.print_hierarchy()
+
+    print()
+    for person in [anne, bill, gerry]:
+        print(person.name, "has", person.count_superiors(), "superiors")
 
 if __name__ == "__main__":
     main()
